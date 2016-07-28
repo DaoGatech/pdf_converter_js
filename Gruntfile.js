c00872b4a810883cb6cb47fb1875be24445b4ee4
DaoGatech <stephenhdao@gatech.edu>
2016-07-28 16:37:06 -0700

    :art: Add jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,17 @@
+/* jshint node: true */
+'use strict';
+
 module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
+        jshint: {
+            all: ['Gruntfile.js', 'src/*.js', 'test/*.js']
         }
     });
-
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('default', ['test']);
 
 };
