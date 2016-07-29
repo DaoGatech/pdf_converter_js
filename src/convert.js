a338a2f54965786f4cb01d11dbb4a6232508f8aa
DaoGatech <stephenhdao@gatech.edu>
2016-07-28 17:00:38 -0700

    :art: Add basic parser

---
+++ src/convert.js
@@ -0,0 +1,177 @@
+/*
+    Helper Functions to convert html elements to pdf
+ */
+var parseContainer = function(cnt, e, p, styles) {
+    var elements = []
+    var children = e.childNodes
+    var i
+    if (children.length !== 0) {
+        for (i = 0; i < children.length; i++) {
+            p = parseElement(elements, children[i], p, styles)
+        }
+    }
+    if (elements.length !== 0) {
+        for (i = 0; i < elements.length; i++) {
+            cnt.push(elements[i])
+        }
+    }
+    return p
+}
+
+var computeStyle = function(o, styles) {
+    for (var i = 0; i < styles.length; i++) {
+        var st = styles[i].trim().toLowerCase().split(':')
+        if (st.length === 2) {
+            switch (st[0]) {
+                case 'font-size':
+                    o.fontSize = 12
+                    break
+                case 'text-align':
+                    switch (st[1]) {
+                        case 'right':
+                            o.alignment = 'right'
+                            break
+                        case 'center':
+                            o.alignment = 'center'
+                            break
+                    }
+                    break
+                case 'font-weight':
+                    switch (st[1]) {
+                        case 'bold':
+                            o.bold = true
+                            break
+                    }
+                    break
+                case 'text-decoration':
+                    switch (st[1]) {
+                        case 'underline':
+                            o.decoration = 'underline'
+                            break
+                    }
+                    break
+                case 'font-style':
+                    switch (st[1]) {
+                        case 'italic':
+                            o.italics = true
+                            break
+                    }
+                    break
+            }
+        }
+    }
+}
+
+var parseElement =  function(cnt, e, p, styles) {
+    if (!styles) {
+        styles = []
+    }
+    var k
+    if (e.getAttribute) {
+        var nodeStyle = e.getAttribute('style')
+        if (nodeStyle) {
+            var ns = nodeStyle.split(';')
+            for (k = 0; k < ns.length; k++) {
+                styles.push(ns[k])
+            }
+        }
+    }
+    switch (e.nodeName.toLowerCase()) {
+        case '#text':
+            var t = {
+                text: e.textContent.replace(/\n/g, '')
+            }
+            if (styles) {
+                computeStyle(t, styles)
+            }
+            p.text.push(t)
+            break
+        case 'b':
+        case 'strong':
+            parseContainer(cnt, e, p, styles.concat(['font-weight:bold']))
+            break
+        case 'u':
+            parseContainer(cnt, e, p, styles.concat(['text-decoration:underline']))
+            break
+        case 'i':
+            parseContainer(cnt, e, p, styles.concat(['font-style:italic']))
+            break
+        case 'span':
+            parseContainer(cnt, e, p, styles)
+            break
+        case 'br':
+            p = createParagraph()
+            cnt.push(p)
+            break
+        case 'table':
+            t = {
+                table: {
+                    widths: [],
+                    body: []
+                }
+            }
+            parseContainer(t.table.body, e, p, styles)
+            for (k = 0; k < 7; k++) {
+                t.table.widths.push('*')
+            }
+            cnt.push(t)
+            break
+        case 'tbody':
+            parseContainer(cnt, e, p, styles)
+            break
+        case 'tr':
+            var row = [];
+            parseContainer(row, e, p, styles)
+            cnt.push(row)
+            break
+        case 'td':
+            p = createParagraph()
+            var st = {
+                stack: []
+            }
+            st.stack.push(p)
+            var rspan = e.getAttribute('rowspan')
+            if (rspan) {
+                st.rowSpan = parseInt(rspan)
+            }
+            var cspan = e.getAttribute('colspan')
+            if (cspan) {
+                st.colSpan = parseInt(cspan)
+            }
+            st.alignment = 'center'
+            parseContainer(st.stack, e, p, styles)
+            cnt.push(st)
+            break
+        case 'div':
+        case 'p':
+            p = createParagraph()
+            st = {
+                stack: [],
+                alignment:'justify',
+                margin: [0,12,0,0]
+            }
+            st.stack.push(p)
+            computeStyle(st, styles)
+            parseContainer(st.stack, e, p)
+            cnt.push(st)
+            break
+        default:
+            break
+    }
+    return p
+}
+
+var parseHTML = function(cnt, htmlText) {
+    var html = $(htmlText.replace(/\t/g, '').replace(/\n/g, '').replace(/\s\s+/g, ' '))
+    var p = createParagraph()
+    for (var i = 0; i < html.length; i++) {
+        parseElement(cnt, html.get(i), p)
+    }
+}
+
+var createParagraph = function() {
+    var p = {
+        text: []
+    }
+    return p
+}
