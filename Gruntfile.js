958acfb25d12255b9e1f9391c083310c82bc5ab8
DaoGatech <stephenhdao@gatech.edu>
2016-08-16 16:17:18 -0700

    :white_check_mark: Add Jasmine/Karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,10 +14,22 @@ module.exports = function(grunt) {
                     jshintrc: '.jshintrc'
                 }
             }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+            },
+            //continuous integration mode: run tests once in PhantomJS browser.
+            continuous: {
+                configFile: 'karma.conf.js',
+                singleRun: true,
+                browsers: ['PhantomJS']
+            },
         }
-    });
+    })
     grunt.loadNpmTasks('grunt-contrib-jshint')
+    grunt.loadNpmTasks('grunt-karma')
     grunt.registerTask('test', ['jshint'])
     grunt.registerTask('default', ['test'])
 
-};
+}
---
+++ test/convertSpec.js
@@ -0,0 +1,5 @@
+describe('init test', function() {
+    it('should be true', function() {
+        expect(true).toBe(true)
+    })
+})
